4f52df60064e789a41af1b359c4d2da43d8ae787
Bruno Philipe <brunophilipe@me.com>
2017-06-25 16:42:09 +0200

    Initial Commit

---
+++ KeyCommandKit/KeyCommandKit.h
@@ -0,0 +1,19 @@
+//
+//  KeyCommandKit.h
+//  KeyCommandKit
+//
+//  Created by Bruno Philipe on 25/6/17.
+//  Copyright © 2017 Bruno Philipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyCommandKit.
+FOUNDATION_EXPORT double KeyCommandKitVersionNumber;
+
+//! Project version string for KeyCommandKit.
+FOUNDATION_EXPORT const unsigned char KeyCommandKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyCommandKit/PublicHeader.h>
+
+
