70fcb0634e3f6dbc491da41ab3e6a23148645262
Bruno Philipe <git@bruno.ph>
2018-02-20 02:08:53 +0100

    Change license from MIT to LGPLv3

--- KeyCommandKit/KeyBindingsRegistry.swift
+++ KeyCommandKit/KeyBindingsRegistry.swift
@@ -1,9 +1,22 @@
 //
 //  KeyBindingsRegistry.swift
-//  KeyCommandKit
+//  KeyCommandKit - Provides customizable key commands to iOS Apps
 //
 //  Created by Bruno Philipe on 25/6/17.
-//  Copyright © 2017 Bruno Philipe. All rights reserved.
+//  Copyright (C) 2017  Bruno Philipe <git@bruno.ph>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import Foundation
